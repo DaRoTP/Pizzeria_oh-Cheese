d4d58c1716e4835f8fa09e004d4934d3f8e8ba9b
Darek <DRodzewicz@gmail.com>
2019-04-07 22:36:53 +0200

    first commit added Login FXML with styling

---
+++ Pizzeria/src/sample/Main.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/View/Login/Login.fxml"));
+        primaryStage.setTitle("Pizzeria - oh Cheese!");
+        Image logo_icon = new Image("/View/Global_Resources/Logo.png");
+        primaryStage.getIcons().add(logo_icon);
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().addAll("/View/Login/Login_style.css");
+
+        primaryStage.setMinWidth(720);
+        primaryStage.setMinHeight(720);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
