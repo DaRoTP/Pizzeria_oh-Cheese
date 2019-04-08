145901623a7d753e8b60414f6f0a36541ce32bce
Darek <DRodzewicz@gmail.com>
2019-04-08 16:06:29 +0200

    added Customer window with styling and a seperate adress window

--- Pizzeria/src/sample/Controller.java
+++ Pizzeria/src/sample/Controller.java
@@ -1,9 +1,14 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
 
     public void say()
     {
         System.out.println("lox");
     }
+    @FXML
+    Button logout = new Button();
 }
