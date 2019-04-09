bd1f42742b835a35e50b03d3dd00785f8c94849f
Darek <DRodzewicz@gmail.com>
2019-04-09 16:43:07 +0200

    styling for Login Scene

--- Pizzeria/src/controller/LoginControl.java
+++ Pizzeria/src/controller/LoginControl.java
@@ -13,13 +13,21 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class LoginControl implements Initializable{
 
     @FXML private ChoiceBox<String> Mode_choice = new ChoiceBox<>();
+	@FXML TextField usernameField = new TextField();
+	@FXML PasswordField  passwordField = new PasswordField();
+	@FXML Label Warning_label = new Label();
 	
+	
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1)
     {
@@ -29,14 +37,44 @@ public class LoginControl implements Initializable{
     }
 	
     public void ChangeScene(ActionEvent event) throws IOException {
-        Parent extended_calculator = FXMLLoader.load(getClass().getResource("/View/Customer/Customer.fxml"));
-        Scene scene = new Scene(extended_calculator);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setScene(scene);
-        window.show();
-        scene.getStylesheets().clear();
-        scene.getStylesheets().add("/View/Customer/Customer.css");
+    	
+    	if(checkIfEmpty()) {
+            Parent extended_calculator = FXMLLoader.load(getClass().getResource("/View/Customer/Customer.fxml"));
+            Scene scene = new Scene(extended_calculator);
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+            scene.getStylesheets().clear();
+            scene.getStylesheets().add("/View/Customer/Customer.css");
+    	}
+    	else
+    	{
+//    		System.out.println("too bad");
+    	}
+
     }
+    
+    public boolean checkIfEmpty() {
+		if (usernameField.getText() == null || usernameField.getText().trim().isEmpty()){
+			usernameField.getStyleClass().add("warning");
+			Warning_label.setText("Bad Username");
+			return false;
+		}
+		else
+		{
+			usernameField.getStyleClass().remove("warning");
+		}
+		if (passwordField.getText() == null || passwordField.getText().trim().isEmpty()){
+			passwordField.getStyleClass().add("warning");
+			Warning_label.setText("Bad Password");
+			return false;
+		}
+		else
+		{
+			passwordField.getStyleClass().remove("warning");
+		}
+		return true;
+	}	
     
     public void SignInOpen(ActionEvent event) throws IOException {
         Parent nn = FXMLLoader.load(getClass().getResource("/View/SignIn/SignIn.fxml"));
